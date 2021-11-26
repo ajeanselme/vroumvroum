e9823ca267a2aabaf84b22e45693480cda462cc4
crimson-king-5 <65815180+crimson-king-5@users.noreply.github.com>
2021-11-26 15:12:01 +0100

    Car choice

--- Assets/_VroumVroum/Scenes/Karim/Scripts/CarSlectors/P3.cs
+++ Assets/_VroumVroum/Scenes/Karim/Scripts/CarSlectors/P3.cs
@@ -2,20 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarSelecting : MonoBehaviour
+public class P3 : MonoBehaviour
 {
     public int currentCarIndex;
     public GameObject[] carModels;
-    
+
     void Start()
     {
-        currentCarIndex = PlayerPrefs.GetInt("SelectedCar", 0);
+        currentCarIndex = PlayerPrefs.GetInt("SelectedCarP3", 0);
         foreach (GameObject car in carModels) car.SetActive(false);
 
         carModels[currentCarIndex].SetActive(true);
     }
 
- 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ChangePrevious();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            ChangeNext();
+        }
+    }
 
     public void ChangeNext()
     {
@@ -24,7 +34,7 @@ public class CarSelecting : MonoBehaviour
         if (currentCarIndex == carModels.Length) currentCarIndex = 0;
 
         carModels[currentCarIndex].SetActive(true);
-        PlayerPrefs.SetInt("SelectedCar", currentCarIndex);
+        PlayerPrefs.SetInt("SelectedCarP3", currentCarIndex);
         PlayerPrefs.Save();
     }
 
@@ -32,12 +42,11 @@ public class CarSelecting : MonoBehaviour
     {
         carModels[currentCarIndex].SetActive(false);
         currentCarIndex--;
-        if (currentCarIndex == -1) currentCarIndex = carModels.Length -1;
+        if (currentCarIndex == -1) currentCarIndex = carModels.Length - 1;
 
         carModels[currentCarIndex].SetActive(true);
-        PlayerPrefs.SetInt("SelectedCar", currentCarIndex);
+        PlayerPrefs.SetInt("SelectedCarP3", currentCarIndex);
         PlayerPrefs.Save();
         Debug.LogError("Previous");
     }
-
 }
